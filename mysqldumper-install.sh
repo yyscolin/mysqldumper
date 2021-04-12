66e67d3712ad2244a0adc20fa588c2273a2dd990
yyscolin <ye.colin@msn.com>
2021-04-12 14:00:34 +0800

    Updated mysqldump.sh to integrate Google Drive and introduced presets

--- mysqldumper-install.sh
+++ mysqldumper-install.sh
@@ -193,21 +193,12 @@ echo '#!/bin/bash
 
 DATE=$(date +%Y-%m-%d)
 TIME=$(date +%H%M)
+DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DIR="$(pwd)"
-VERSION="MySqlDump Script for Linux by Colin Ye - v1.0 (5th April 2021)"
+VERSION="MySqlDump Script for Linux by Colin Ye - v1.01 (12th April 2021)"
 
-function housekeep() {
-    FOLDERS=$(ls $backup_dir | grep -E "^MYSQL\.$1\.[0-9]{4}-[0-9]{2}-[0-9]{2}\.[0-9]{4}\.$2\.7z$")
-    CURRENT_COUNT=$(echo $FOLDERS | wc -w)
-    REMOVE_COUNT=$((CURRENT_COUNT-backup_count))
-
-    if [ $REMOVE_COUNT -gt 0 ]; then
-        FILES_TO_DELETE=$(echo $FOLDERS | cut -d\  -f-$((REMOVE_COUNT)))
-        for file in $FILES_TO_DELETE; do
-            rm "$backup_dir/$file"
-        done
-    fi
-}
+VALUES_UPLOAD_TO="google"
+VALUES_BACKUP_TYPE="full split"
 
 # Check for dependencies
 if ! command -v 7z &>/dev/null; then
@@ -215,133 +206,328 @@ if ! command -v 7z &>/dev/null; then
     exit 1
 fi
 
-# Set options
-option_key=""
-backup_count=0
+# Default options
+backup_count=0 #no housekeep
 backup_dir="$DIR"
 backup_type=full
-zip_pass=""
+
+# Read arguments
+option_key=""
 
 for arg in "$@"; do
-    if [ "$option_key" == "-t" ] || [ "$option_key" == "--type" ]; then
-        if [ "$arg" == "full" ] ||  [ "$arg" == "split" ] || [ "$arg" == "both" ]; then
-            backup_type=$arg
-            option_key=""
-        else
-            echo Invalid backup type. Run the command with -h or --help for more details.
-            exit 1
-        fi
-    elif [ "$option_key" == "-d" ] || [ "$option_key" == "--dir" ]; then
-        if [[ ${arg:0:1} == "/" ]]; then
-            backup_dir="$arg"
-        else
-            backup_dir="$DIR/$arg"
-        fi
-        option_key=""
-    elif [ "$option_key" == "-k" ] || [ "$option_key" == "--housekeep" ]; then
-        if [[ ! "$arg" =~ ^[0-9]+$ ]]; then
-            echo Invalid option for $option_key. Run the command with -h or --help for more details.
-            exit 1
-        fi
-        backup_count=$arg
-        option_key=""
-    elif [ "$option_key" == "-z" ] || [ "$option_key" == "--zip-pass" ]; then
-        if [ ${#arg} -le 4 ]; then
-            echo Zip password must be longer than 4 characters.
-            exit 1
-        fi
-        zip_pass=-p"$arg"
-        option_key=""
-    elif [ "$arg" == "-d" ] || [ "$arg" == "--dir" ]; then
+    # Set option key
+    if [ "$arg" == "-d" ] || [ "$arg" == "--dir" ]; then
         option_key="$arg"
-        backup_dir=""
+        key_backup_dir="$arg"
     elif [ "$arg" == "-h" ] || [ "$arg" == "--help" ]; then
         echo -e "$VERSION\n"
         echo "Arguments"
-        echo "    -d --dir <path>              Write the backup file to a different directory"
-        echo "    -h --help                    Display the help section and exit"
-        echo "    -k --housekeep <#>           Keep only the latest # copies in the directory"
-        echo "    -t --type <full|split>       Specify the backup type; Default is \`full\` if not specified"
-        echo "    -v --version                 Display the version details and exit"
-        echo "    -z --zip-pass <password>     Set the password to protect the 7z archive"
+        echo "    -u --upload-to {google}          Upload a copy of the created backup file to a cloud drive"
+        echo "    -d --dir <directory>             Create the backup file in the specified directory"
+        echo "    -h --help                        Display the help section and exit"
+        echo "    -k --housekeep <#>               Keep only the latest # copies in the backup directory"
+        echo "    -p --profile                     Use the settings from the specified preset file"
+        echo "    -r --remove-local                Remove the local copy after uploading to cloud drive"
+        echo "    -t --type {full|split}           Specify the backup type; Default is \`full\` if not specified"
+        echo "    -v --version                     Display the version details and exit"
+        echo "    -z --zip-pass <password>         Set the password to protect the 7z archive"
         exit 0
     elif [ "$arg" == "-k" ] || [ "$arg" == "--housekeep" ]; then
         option_key="$arg"
-        backup_count=""
+        key_backup_count="$arg"
+    elif [ "$arg" == "-p" ] || [ "$arg" == "--profile" ]; then
+        option_key="$arg"
+        key_backup_profile="$arg"
+    elif [ "$arg" == "-r" ] || [ "$arg" == "--remove-local" ]; then
+        remove_local=1
     elif [ "$arg" == "-t" ] || [ "$arg" == "--type" ]; then
         option_key=$arg
-        backup_type=""
+        key_backup_type="$arg"
+    elif [ "$arg" == "-u" ] || [ "$arg" == "--upload-to" ]; then
+        option_key="$arg"
+        key_upload_to="$arg"
     elif [ "$arg" == "-v" ] || [ "$arg" == "--version" ]; then
         echo "$VERSION"
         exit 0
     elif [ "$arg" == "-z" ] || [ "$arg" == "--zip-pass" ]; then
         option_key=$arg
-        zip_pass=0
+        key_zip_pass="$arg"
+
+    # Set option value
+    elif [ "$option_key" == "-d" ] || [ "$option_key" == "--dir" ]; then
+        option_key=""
+        if [[ ${arg:0:1} == "/" ]]; then
+            arg_backup_dir="$arg"
+        else
+            arg_backup_dir="$DIR/$arg"
+        fi
+    elif [ "$option_key" == "-k" ] || [ "$option_key" == "--housekeep" ]; then
+        option_key=""
+        arg_backup_count=$arg
+    elif [[ "$arg" == -k* ]]; then
+        key_backup_count=-k
+        arg_backup_count=${arg:2}
+    elif [ "$option_key" == "-p" ] || [ "$option_key" == "--profile" ]; then
+        option_key=""
+        if [[ ${arg:0:1} == "/" ]]; then
+            backup_profile="$arg"
+        else
+            backup_profile="$DIR/$arg"
+        fi
+    elif [ "$option_key" == "-t" ] || [ "$option_key" == "--type" ]; then
+        option_key=""
+        arg_backup_type=$arg
+    elif [[ "$arg" == -t* ]]; then
+        key_backup_type=-t
+        arg_backup_type=${arg:2}
+    elif [ "$option_key" == "-u" ] || [ "$option_key" == "--upload-to" ]; then
+        option_key=""
+        arg_upload_to="$arg"
+    elif [[ "$arg" == -u* ]]; then
+        key_upload_to=-u
+        arg_upload_to=${arg:2}
+    elif [ "$option_key" == "-z" ] || [ "$option_key" == "--zip-pass" ]; then
+        option_key=""
+        arg_zip_pass="$arg"
+    elif [[ "$arg" == -z* ]]; then
+        key_zip_pass=-z
+        arg_zip_pass=${arg:2}
+
+    # Invalid argument
     else
         echo Invalid argument $arg. Run the command with -h or --help for more details.
         exit 1
     fi
 done
 
-# Check for errors
-if [ "$backup_dir" == "" ]; then
-    echo Please specify the backup directory. Run the command with -h or --help for more details.
+# Check for input errors
+if [ "$key_backup_dir" != "" ] && [ "$arg_backup_dir" == "" ]; then
+    echo Please specify the backup directory. Or omit the $key_backup_dir switch.
+    echo Run the command with -h or --help for more details.
     exit 1
-elif [ ! -d "$backup_dir" ]; then
+elif [ "$key_backup_type" != "" ] && [ "$arg_backup_type" == "" ]; then
+    echo Please specify the backup type. Or omit the $key_backup_type switch.
+    echo Run the command with -h or --help for more details.
+    exit 1
+elif [ "$key_backup_count" != "" ] && [ "$arg_backup_count" == "" ]; then
+    echo Please specify the number of backup copies to keep. Or omit the $key_backup_count switch.
+    echo Run the command with -h or --help for more details.
+    exit 1
+elif [ "$key_backup_profile" != "" ] && [ "$backup_profile" == "" ]; then
+    echo Please specify the backup profile. Or omit the $key_backup_profile switch.
+    echo Run the command with -h or --help for more details.
+    exit 1
+elif [ "$key_upload_to" != "" ] && [ "$arg_upload_to" == "" ]; then
+    echo Please specify the upload destination. Or omit the $key_upload_to switch.
+    echo Run the command with -h or --help for more details.
+    exit 1
+elif [ "$key_zip_pass" != "" ] && [ "$arg_zip_pass" == "" ]; then
+    echo Please specify the password to protect 7z archives. Or omit the $key_zip_pass switch.
+    echo Run the command with -h or --help for more details.
+    exit 1
+elif [ ! -f "$backup_profile" ]; then
+    echo Error: the backup profile \`$backup_profile\` settings file cannot be found.
+    exit 1
+fi
+
+# Get settings from backup profile
+settings_backup_count=$(cat "$backup_profile" | tr -d ' ' | grep ^backup_count=)
+if [ "$settings_backup_count" != "" ]; then
+    backup_count=$(echo $settings_backup_count | head -n1 | cut -d= -f2-)
+fi
+
+settings_backup_dir=$(cat "$backup_profile" | tr -d ' ' | grep ^backup_dir=)
+if [ "$settings_backup_dir" != "" ]; then
+    backup_dir=$(echo $settings_backup_dir | head -n1 | cut -d= -f2-)
+fi
+
+settings_backup_type=$(cat "$backup_profile" | tr -d ' ' | grep ^backup_type=)
+if [ "$settings_backup_type" != "" ]; then
+    backup_type=$(echo $settings_backup_type | head -n1 | cut -d= -f2-)
+fi
+
+settings_upload_to=$(cat "$backup_profile" | tr -d ' ' | grep ^upload_to=)
+if [ "$settings_upload_to" != "" ]; then
+    upload_to=$(echo $settings_upload_to | head -n1 | cut -d= -f2-)
+fi
+
+settings_remove_local=$(cat "$backup_profile" | tr -d ' ' | grep ^remove_local=)
+if [ "$settings_remove_local" != "" ]; then
+    remove_local=$(echo $settings_remove_local | head -n1 | cut -d= -f2-)
+fi
+
+settings_zip_pass=$(cat "$backup_profile" | tr -d ' ' | grep ^zip_pass= | head -n1 | cut -d= -f2-)
+
+# Overwrite settings with values from arguments
+if [ "$arg_backup_count" != "" ]; then
+    backup_count="$arg_backup_count"
+fi
+
+if [ "$arg_backup_dir" != "" ]; then
+    backup_dir="$arg_backup_dir"
+fi
+
+if [ "$arg_backup_type" != "" ]; then
+    backup_type="$arg_backup_type"
+fi
+
+if [ "$arg_upload_to" != "" ]; then
+    upload_to="$arg_upload_to"
+fi
+
+if [ "$arg_remove_local" != "" ]; then
+    remove_local="$arg_remove_local"
+fi
+
+# Check for logical errors
+if [ ! -d "$backup_dir" ]; then
     echo Error: the directory \"$backup_dir\" does not exists
     exit 1
 elif [ ! -w "$backup_dir" ]; then
     echo Error: no permission to write to \"$backup_dir\"
     exit 1
-elif [ "$backup_type" == "" ]; then
-    echo Please specify the backup type. Or omit the $option_key switch.
+elif [[ ! ${VALUES_UPLOAD_TO[*]} =~ "$upload_to" ]]; then
+    echo Error: invalid backup destination: \`$upload_to\`.
+    if [ "$key_upload_to" != "" ]; then
+        echo Run the command with -h or --help for more details.
+    fi
+    exit 1
+elif [[ ! ${VALUES_BACKUP_TYPE[*]} =~ "$backup_type" ]]; then
+    echo Error: invalid backup type \`$backup_type\`.
+    if [ "$key_backup_type" != "" ]; then
+        echo Run the command with -h or --help for more details.
+    fi
+    exit 1
+elif [[ ! "$backup_count" =~ ^[0-9]+$ ]]; then
+    echo Error: invalid backup count \`$backup_count\`: must be 0 or a positive integer.
+    if [ "$key_backup_count" != "" ]; then
+        echo Run the command with -h or --help for more details.
+    fi
+    exit 1
+elif [ "$arg_zip_pass" != "" ] && [ ${#arg_zip_pass} -le 4 ]; then
+    echo Error: zip password must be longer than 4 characters.
     echo Run the command with -h or --help for more details.
     exit 1
-elif [ "$backup_count" == "" ]; then
-    echo Please specify the number of backup copies to keep. Or omit the $option_key switch.
-    echo Run the command with -h or --help for more details.
+elif [ "$arg_zip_pass" == "" ] && [ "$settings_zip_pass" != "" ] && [ ${#settings_zip_pass} -le 4 ]; then
+    echo Error: zip password must be longer than 4 characters.
     exit 1
-elif [ "$zip_pass" == "0" ]; then
-    echo Please specify the password to protect 7z archives. Or omit the $option_key switch.
-    echo Run the command with -h or --help for more details.
-    exit 1
+elif [ "$remove_local" == "1" ] && [ "$upload_to" == "" ]; then
+    echo Error: cannot remove local backup file unless uploading a cloud drive.
+fi
+
+# Set derived variables
+if [ "$backup_type" == "full" ]; then
+    ext=sql.7z
+else
+    ext=tar.7z
+fi
+
+if [ "$arg_zip_pass" != "" ]; then
+    zip_pass_opt=-p"$arg_zip_pass"
+elif [ "$settings_zip_pass" != "" ]; then
+    zip_pass_opt=-p"`cat "$backup_profile" | tr -d ' ' | grep ^zip_pass= | head -n1 | cut -d= -f2-`"
+fi
+
+# Init Google API; check errors and get auth token
+if [ "$upload_to" == "google" ]; then
+    if [ "$(cat "$backup_profile" | tr -d ' ' | grep ^client_id=)" == "" ]; then
+        echo Error: Google client_id not specified in $backup_profile
+        exit 1
+    fi
+
+    if [ "$(cat "$backup_profile" | tr -d ' ' | grep ^client_secret=)" == "" ]; then
+        echo Error: Google client_secret not specified in $backup_profile
+        exit 1
+    fi
+
+    if [ "$(cat "$backup_profile" | tr -d ' ' | grep ^refresh_token=)" == "" ]; then
+        echo Error: Google refresh_token not specified in $backup_profile
+        exit 1
+    fi
+
+    if [ "$(cat "$backup_profile" | tr -d ' ' | grep ^folder=)" == "" ]; then
+        echo Error: Google folder not specified in $backup_profile
+        exit 1
+    fi
+    
+    # Delete existing auth_token in config file
+    sed -i '/^auth_token/d' "$backup_profile"
+
+    # Generate new auth_token
+    token_url=https://oauth2.googleapis.com/token
+    auth_token=$(curl -s -d client_id=`cat "$backup_profile" | tr -d ' ' | grep ^client_id= | head -n1 | cut -d= -f2-` -d client_secret=`cat "$backup_profile" | tr -d ' ' | grep ^client_secret= | head -n1 | cut -d= -f2-` -d refresh_token=`cat "$backup_profile" | tr -d ' ' | grep ^refresh_token= | head -n1 | cut -d= -f2-` -d grant_type=refresh_token $token_url | grep '"access_token":' | tr -d " " | cut -d\" -f 4)
+    echo auth_token=$auth_token >> $backup_profile
 fi
 
 # Full backup
 if [ "$backup_type" == "full" ]; then
 	mysqldump=$(mysqldump --no-tablespaces --all-databases) || exit 1
-    echo "$mysqldump" | 7z a -si $zip_pass "$backup_dir/MYSQL.FULL.$DATE.$TIME.sql.7z" &>/dev/null
+    echo "$mysqldump" | 7z a -si $zip_pass_opt "$backup_dir/mysql.$backup_type.$DATE.$TIME.$ext" &>/dev/null
     if [ $? -ne 0 ]; then
         echo Error: the script has encountered an error during the 7z compression process
         exit 1
     fi
-    chmod 640 "$backup_dir/MYSQL.FULL.$DATE.$TIME.sql.7z"
-
-    if [ $backup_count -gt 0 ]; then
-        housekeep FULL sql
-    fi
+    chmod 640 "$backup_dir/mysql.$backup_type.$DATE.$TIME.$ext"
 
 # Split backup
 elif [ "$backup_type" == "split" ]; then
 	databases=$(mysql -e "show databases" | tail -n+2 | grep -v -e information_schema -e mysql -e performance_schema -e sys) || exit 1
 	for db in $databases; do
-		mkdir -p "$backup_dir/MYSQL.$DATE.$TIME"
+		mkdir -p "$backup_dir/mysql.$DATE.$TIME"
 
 		tables=$(mysql -D $db -e "show tables" | tail -n+2) || exit 1
 		for table in $tables; do
-			mysqldump --no-tablespaces $db $table > "$backup_dir/MYSQL.$DATE.$TIME/MYSQL.$DATE.$TIME.$db.$table.sql" || exit 1
+			mysqldump --no-tablespaces $db $table > "$backup_dir/mysql.$DATE.$TIME/mysql.$DATE.$TIME.$db.$table.sql" || exit 1
 		done
 	done
 	
-    tar -C "$backup_dir" -cf - MYSQL.$DATE.$TIME --remove-files | 7z a -si $zip_pass "$backup_dir/MYSQL.SPLIT.$DATE.$TIME.tar.7z" &>/dev/null
+    tar -C "$backup_dir" -cf - mysql.$DATE.$TIME --remove-files | 7z a -si $zip_pass_opt "$backup_dir/mysql.$backup_type.$DATE.$TIME.$ext" &>/dev/null
     if [ $? -ne 0 ]; then
         echo Error: the script has encountered an error during the 7z compression process
         exit 1
     fi
-    chmod 640 "$backup_dir/MYSQL.SPLIT.$DATE.$TIME.tar.7z"
+    chmod 640 "$backup_dir/mysql.$backup_type.$DATE.$TIME.$ext"
+fi
 
-    if [ $backup_count -gt 0 ]; then
-        housekeep SPLIT tar
+# Upload to cloud drives
+if [ "$upload_to" == "google" ]; then
+    curl -s -X POST -H "Authorization: Bearer `cat "$backup_profile" | tr -d ' ' | grep ^auth_token= | head -n1 | cut -d= -f2-`" \
+            -F "metadata={name :'mysql.$backup_type.$DATE.$TIME.$ext', parents: ['`cat "$backup_profile" | tr -d ' ' | grep ^folder= | head -n1 | cut -d= -f2-`']};type=application/json;charset=UTF-8;" \
+            -F "file=@$backup_dir/mysql.$backup_type.$DATE.$TIME.$ext;type=application/zip" \
+            "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart" &>/dev/null
+fi
+
+# Remove local copy
+if [ "$remove_local" == "1" ]; then
+    rm "$backup_dir/mysql.$backup_type.$DATE.$TIME.$ext"
+fi
+
+# Housekeeping
+if [ $backup_count -gt 0 ]; then
+    if [ "$upload_to" == "google" ]; then
+        files=$(curl -s -H "Authorization: Bearer `cat "$backup_profile" | tr -d ' ' | grep ^auth_token= | head -n1 | cut -d= -f2-`" "https://www.googleapis.com/drive/v3/files?orderBy=name&q=%22`cat "$backup_profile" | tr -d ' ' | grep ^folder= | head -n1 | cut -d= -f2-`%22%20in%20parents%20and%20name%20contains%20%22mysql.$backup_type%22" | tr -d ' ' | grep ^\"id\": | cut -d\" -f4 )
+        current_count=$(echo $files | wc -w)
+        remove_count=$((current_count-backup_count))
+
+        if [ $remove_count -gt 0 ]; then
+            files_to_delete=$(echo $files | cut -d\  -f-$((remove_count)))
+            for file_id in $files_to_delete; do
+                curl -s -X DELETE -H "Authorization: Bearer `cat "$backup_profile" | tr -d ' ' | grep ^auth_token= | head -n1 | cut -d= -f2-`" https://www.googleapis.com/drive/v3/files/$file_id
+            done
+        fi
+    fi
+
+    if [ "$remove_local" != "1" ]; then
+        files=$(ls $backup_dir | grep -E "^mysql\.$backup_type\.[0-9]{4}-[0-9]{2}-[0-9]{2}\.[0-9]{4}\.$ext$")
+        current_count=$(echo $files | wc -w)
+        remove_count=$((current_count-backup_count))
+
+        if [ $remove_count -gt 0 ]; then
+            files_to_delete=$(echo $files | cut -d\  -f-$((remove_count)))
+            for file in $files_to_delete; do
+                rm "$backup_dir/$file"
+            done
+        fi
     fi
 fi
 ' > $CRON_USER_HOME/$DUMP_SCRIPT
