d86ab29d4a978dfc7a8e25df4f501eb67ef1de55
yyscolin <ye.colin@msn.com>
2021-05-17 10:58:43 +0800

    major workflow changes

--- mysqldump.sh
+++ mysqldump.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-DIR_SCRIPT="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-VERSION="MySqlDump Script for Linux by Colin Ye - v2 (15th May 2021)"
-REMOTE_DRIVE_VALUES="google"
+declare -A DEFAULTS
+DEFAULTS[backup_type]=full
+DEFAULTS[local_dir]=/var/mysqldumps
+DEFAULTS[local_filename]=mysqldump.%Y-%m-%d.%H%M%S
 
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 backup_profile="$1"
 drives=("")
 
@@ -16,22 +18,10 @@ function throw_error() {
 
 function parse_filename() {
     local filename="$1"
-    local mode=0 # 0 = normal; 1 = regex; 2 = wildcard
-    [ "$2" != "" ] && mode=$2
-
-    [ $mode == 1 ] && filename="${filename//\./\\\.}"
-
     local date_elements="a A b B c C d D e F g G h H I j k l m M n N p P q r R s S t T u U V w W x X y Y z :z ::z :::z Z"
     for x in $date_elements; do
-        if [ $mode == 1 ]; then
-            filename="${filename//\%$x/.*}"
-        elif [ $mode == 2 ]; then
-            filename="${filename//\%$x/\*}"
-        else
-            filename="${filename//\%$x/$(date +%$x)}"
-        fi
+        filename="${filename//\%$x/$(date +%$x)}"
     done
-
     echo $filename
 }
 
@@ -71,22 +61,21 @@ function read_rdrive_profile() {
     drives+=("remote_type=$remote_type\nauth_token=$auth_token\n")
 }
 
-if [ "$backup_profile" == "" ]; then
-    throw_error missing argument: backup profile name
-else
-    backup_profile_file="$DIR_SCRIPT/settings/backup_profiles/$backup_profile"
-    [ ! -f "$backup_profile_file" ] && throw_error missing file: $backup_profile_file
-fi
+[ "$backup_profile" == "" ] && throw_error missing argument: backup profile name
+
+backup_profile_file="$dir/settings/backup_profiles/$backup_profile"
+[ ! -f "$backup_profile_file" ] && throw_error missing file: $backup_profile_file
 
 if ! command -v 7z &>/dev/null; then
     throw_error required package missing: p7zip-full
 fi
 
+DEFAULTS[local_dir]="${DEFAULTS[local_dir]}/$backup_profile"
+
 while read line; do
-    line_count=$(( line_count+1 ))
     if [[ $line =~ ^\[.*\]*$ ]]; then
         rdrive_profile_name=${line:1:-1}
-        read_rdrive_profile "$DIR_SCRIPT/settings/remote_drive_profiles/$rdrive_profile_name"
+        read_rdrive_profile "$dir/settings/remote_drive_profiles/$rdrive_profile_name"
     elif [[ $line =~ ^[^\#].+$ ]]; then
         drives[-1]=${drives[-1]}"$line\n"
     fi
@@ -97,44 +86,36 @@ for ((i = 0; i < ${#drives[@]}; i++)); do
         parameters="backup_type local_count local_dir local_filename zip_pass"
         for key in $parameters; do
             value="$(echo -e "${drives[$i]}" | grep ^$key= | tail -n1 | cut -d= -f2-)"
+            [ "$value" == "" ] && value="${DEFAULTS[$key]}"
             printf -v $key %s "$value"
         done
-        [ "$backup_type" == "" ] && backup_type=full
-        [ "$local_dir" == "" ] && local_dir="$DIR_SCRIPT/mysqldumps"
-        [ ${local_dir:0:1} != "/" ] && local_dir=$DIR_SCRIPT/$local_dir
-        [ "$local_filename" == "" ] && local_filename="mysqldumper.%Y-%m-%d.%H%M%S"
-        local_filename_parsed=$(parse_filename $local_filename)
-        [ "$zip_pass" != "" ] && zip_pass=-p$zip_pass
-        
-        if [ $backup_type == "full" ]; then
-            mysqldump --defaults-extra-file="$DIR_SCRIPT/my.cnf" --no-tablespaces --all-databases > "$local_dir/$local_filename_parsed.sql" || exit 1
-            tar -C "$local_dir" -cf - $local_filename_parsed.sql --remove-files | 7z a -si "$zip_pass" "$local_dir/$local_filename_parsed.tar.7z" &>/dev/null
-            if [ $? -ne 0 ]; then
-                echo mysqldumper: error: process failure: 7z compression
-                exit 1
-            fi
+        [ ${local_dir:0:1} != "/" ] && local_dir=$dir/$local_dir
+        if [ ! -d "$local_dir" ]; then
+            mkdir -p "$local_dir" || exit 1
+        fi
+        [ "$zip_pass" == "" ] && throw_error zip files must be protected by password
+        [[ $zip_pass =~ \  ]] && throw_error illegal character: spaces are not allowed for zip password
 
+        DEFAULTS[remote_filename]="$local_filename"
+
+        local_filename=$(parse_filename $local_filename)
+        mkdir -p "$local_dir/$local_filename"
+        if [ $backup_type == "full" ]; then
+            mysqldump --defaults-extra-file="$dir/my.cnf" --no-tablespaces --all-databases > "$local_dir/$local_filename/$local_filename.sql" || exit 1
         elif [ $backup_type == "split" ]; then
             databases=$(mysql -e "show databases" | tail -n+2 | grep -v -e information_schema -e mysql -e performance_schema -e sys) || exit 1
             for db in $databases; do
-                mkdir -p "$local_dir/$local_filename_parsed"
-
                 tables=$(mysql -D $db -e "show tables" | tail -n+2) || exit 1
                 for table in $tables; do
-                    mysqldump --defaults-extra-file="$DIR_SCRIPT/my.cnf" --no-tablespaces $db $table > "$local_dir/$local_filename_parsed/$db.$table.sql" || exit 1
+                    mysqldump --defaults-extra-file="$dir/my.cnf" --no-tablespaces $db $table > "$local_dir/$local_filename/$db.$table.sql" || exit 1
                 done
             done
-            
-            tar -C "$local_dir" -cf - $local_filename_parsed --remove-files | 7z a -si "$zip_pass" "$local_dir/$local_filename_parsed.tar.7z" &>/dev/null
-            if [ $? -ne 0 ]; then
-                echo mysqldumper: error: process failure: 7z compression
-                exit 1
-            fi
-        
         else
-            echo mysqldumper: error: invalid value: backup profile settings: backup type: $backup_type
-            exit 1
+            throw_error "invalid setting(s): $backup_profile_file: backup type: $backup_type"
         fi
+
+        tar -C "$local_dir" -cf - $local_filename --remove-files | 7z a -si -p$zip_pass "$local_dir/$local_filename.tar.7z" &>/dev/null
+        [ $? -ne 0 ] && throw_error 7z compression failed
     else # Remote upload
         parameters="remote_type auth_token remote_filename remote_count remote_folder"
         for key in $parameters; do
@@ -142,20 +123,18 @@ for ((i = 0; i < ${#drives[@]}; i++)); do
             printf -v $key %s "$value"
         done
 
-        [ "$remote_count" == "" ] && remote_count=$local_count
-        [ "$remote_folder" != "" ] && remote_folder_parsed=", parents: [\"$remote_folder\"]" || remote_folder_parsed=""
-        [ "$remote_filename" == "" ] && remote_filename=$local_filename
-        remote_filename_parsed=$(parse_filename "$remote_filename")
-        remote_filename_wildcard=$(parse_filename "$remote_filename" 2)
+        [ "$remote_folder" == "" ] && remote_folder_parsed="" || remote_folder_parsed=", parents: [\"$remote_folder\"]"
+        [ "$remote_filename" == "" ] && remote_filename=${DEFAULTS[remote_filename]}
+        remote_filename=$(parse_filename "$remote_filename")
 
         curl -s -X POST -H "Authorization: Bearer $auth_token" \
-                -F "metadata={name :\"$remote_filename_parsed.tar.7z\"$remote_folder_parsed};type=application/json;charset=UTF-8;" \
-                -F "file=@$local_dir/$local_filename_parsed.tar.7z;type=application/zip" \
+                -F "metadata={name :\"$remote_filename.tar.7z\"$remote_folder_parsed};type=application/json;charset=UTF-8;" \
+                -F "file=@$local_dir/$local_filename.tar.7z;type=application/zip" \
                 "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart" &>/dev/null
 
         if [ $remote_count != "" ]; then
             [ "$remote_folder" == "" ] && remote_folder=root
-            files_url="https://www.googleapis.com/drive/v3/files?orderBy=name&q=%22$remote_folder%22%20in%20parents%20and%20name%20contains%20%22$remote_filename_wildcard%22"
+            files_url="https://www.googleapis.com/drive/v3/files?orderBy=name&q=%22$remote_folder%22%20in%20parents%20and%20mimeType%20=%20%22application/zip%22"
             files=$(curl -s -H "Authorization: Bearer $auth_token" $files_url | tr -d " " | grep ^\"id\": | cut -d\" -f4 )
             current_count=$(echo $files | wc -w)
             remove_count=$((current_count-remote_count))
@@ -172,7 +151,7 @@ done
 # Local housekeeping
 if [ "$local_count" != "" ]; then
     local_filename_regexp=$(parse_filename $local_filename 1)
-    files=$(ls $local_dir | grep -E "^$local_filename_regexp\.tar\.7z$")
+    files=$(ls "$local_dir" | grep .tar.7z$)
     current_count=$(echo $files | wc -w)
     remove_count=$((current_count-local_count))
 
